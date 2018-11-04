1d1f1e603d114ad32f35db0f0c2a3b76288086c7
Petro Zubar <pmzubar@gmail.com>
2018-11-04 22:15:02 +0200

    Router added

---
+++ components/Router.php
@@ -0,0 +1,52 @@
+<?php
+
+class Router
+{
+    private $routes;
+
+    public function __construct()
+    {
+        $routesPath = ROOT.'/config/routes.php';
+        $this->routes = include($routesPath);
+    }
+
+//    Returns request string
+    private function getURI()
+    {
+        if (!empty($_SERVER['REQUEST_URI'])) {
+            return trim($_SERVER['REQUEST_URI'], '/');
+        }
+        return '';
+    }
+
+    public  function run()
+    {
+        //get query string
+        $uri = $this->getURI();
+        //check existing this query in routes.php
+        foreach ($this->routes as $uriPattern => $path) {
+
+            if (preg_match("~$uriPattern~", $uri))
+            {
+                //get which controller and action works on this query
+                $segments = explode('/', $path);
+
+                $controllerName = array_shift($segments).'Controller';
+                $controllerName = ucfirst($controllerName);
+
+                $actionName = 'action'.ucfirst(array_shift($segments));
+
+                $controllerFile = ROOT.'/controllers/'.$controllerName.'.php';
+                if (file_exists($controllerFile))
+                    include_once($controllerFile);
+
+                $controllerObject = new $controllerName;
+                $result = $controllerObject->$actionName();
+
+                if ($result != null)
+                    break;
+            }
+        }
+
+    }
+}
--- index.php
+++ index.php
@@ -5,5 +5,22 @@
  * Date: 04.11.2018
  * Time: 21:43
  */
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+//
+define('ROOT', dirname(__FILE__));
+//
+if (!file_exists(ROOT."config/success"))
+{
+//    echo  ROOT . '/views/install_form.php';
+    if (file_exists(ROOT . '/views/install_form.php'))
+        header('Location:' .'/views/install_form.php');
+}
+    //{
+//    echo 'a';
+//    header('Location: ' . ROOT . '/views/install_form.php');
+//}
 
-phpinfo();
+//var_dump($_SERVER);
+
+//phpinfo();

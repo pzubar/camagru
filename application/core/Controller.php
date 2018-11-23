872b0238d1e6ffe61fbc8c94b505e570e75ac315
Petro Zubar <pmzubar@gmail.com>
2018-11-23 23:33:37 +0200

    model class added

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -13,9 +13,20 @@ abstract class Controller
 	public $route;
 	public $view;
 
-	public function __construct($route)
+	public function __construct(array $route)
 	{
 		$this->route = $route;
 		$this->view = new View($route);
+		$this->loadModel($route['controller']);
+	}
+	
+	public function loadModel(string $name) {
+		$path = 'application\models\\'.ucfirst($name);
+		if (class_exists($path)) {
+			return new $path;
+		} else {
+			View::errorCode(503);
+		}
+		debug($path);
 	}
 }

08cbea40cf106d668fbe281d4da6d63c143afe48
Petro Zubar <pmzubar@gmail.com>
2018-11-30 17:41:26 +0200

    login page ui improved

--- application/views/account/login.php
+++ application/views/account/login.php
@@ -1,8 +1,26 @@
-<h3>Sign in</h3>
-<form action="/account/login" method="post">
-	<label for="login">Login</label>
-	<input id="login" type="text">
-	<label for="password">Password</label>
-	<input id="password" type="password">
-    <button type="submit">Sign In</button>
-</form>
+<style>
+    .login-form {
+        height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+</style>
+
+<div class="container">
+    <div class="row justify-content-md-center login-form">
+        <div class="col-md-auto">
+            <h3>Sign in</h3>
+            <form action="/account/login" method="post">
+                <div class="form-group">
+                    <label for="login">Login</label>
+                    <input class="form-control" id="login" type="text">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input class="form-control" id="password" type="password">
+                </div>
+                <button class="btn" type="submit">Sign In</button>
+            </form>
+        </div>
+    </div>
+</div>

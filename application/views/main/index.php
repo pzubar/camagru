bc82b9ae757255d2d627ea005c0fe487f8c0508e
pzubar <pmzubar@gmail.com>
2018-12-16 00:45:25 +0200

    minor config improvements

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -1,11 +1,11 @@
 <p>Главная страница</p>
 
 
-    <?php foreach ($news as $val):?>
-    <h3>
-        <?php echo $val['h1']?>
-    </h3>
-    <p>
-        <?php echo $val['description']?>
-    </p>
-    <?php endforeach; ?>
+<!--    --><?php //foreach ($news as $val):?>
+<!--    <h3>-->
+<!--        --><?php //echo $val['h1']?>
+<!--    </h3>-->
+<!--    <p>-->
+<!--        --><?php //echo $val['description']?>
+<!--    </p>-->
+<!--    --><?php //endforeach; ?>

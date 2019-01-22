ca0935c246c56c1c773022f65b5b13c97b6218bf
pzubar <pmzubar@gmail.com>
2019-01-22 21:35:24 +0200

    MAMPIFICATION RETURNED :<

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,6 @@
 <?php
-	$DB_DSN = 'mysql:host=docker-mysql;port=3306';
+//	$DB_DSN = 'mysql:host=docker-mysql;port=3306';
+	$DB_DSN = 'mysql:host=localhost;port=3306';
 	$DB_USER = 'root';
 	$DB_PASSWORD = 'rootpwd';
 	$DB_NAME = 'camagru';
--- archive/login/register-form.php
+++ archive/login/register-form.php
@@ -11,16 +11,6 @@
 		<form action="account_creation.php" method="post">
 			<div id="top-bar">Create your account<a href="../index.php" class="close">&otimes;</a></div>
 			<?php
-			/*
-				if ($_GET['loginErr'] == 1)
-					echo "<div class=\"errvis\">Check the input fields!</div>";
-				else if ($_GET['loginErr'] == 2)
-					echo "<div class=\"errvis\">User with same account login exists. Please change your login!</div>";
-				else if ($_GET['loginErr'] == 3)
-					echo "<div class=\"errvis\">ACHTUNG!!!</div>";
-				else
-					echo "<div class=\"errhide\">Check the input fields!</div>";
-			*/
 			?>
 			<input type="text" name="username" placeholder="Username" /><br />
 			<input type="password" name="password" placeholder="Password" /><br />

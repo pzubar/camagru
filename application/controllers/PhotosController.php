dc941e65396f0cafa91a04cc834f9532bdc7dbb5
pzubar <pmzubar@gmail.com>
2019-01-22 22:37:26 +0200

    photos merging method adjusted

--- application/controllers/PhotosController.php
+++ application/controllers/PhotosController.php
@@ -10,7 +10,23 @@ class PhotosController extends Controller
 //        $vars = [
 //            'news' => $result
 //        ];
-        $this->view->render('New Photo');
+//        debug(gd_info());
+//        $this->view->render('New Photo');
+//        debug('la');
+        $dest = imagecreatefrompng('application/controllers/b.png');
+        $src = imagecreatefromjpeg('application/controllers/a.jpg');
+//
+        imagealphablending($dest, false);
+        imagesavealpha($dest, true);
+//
+        imagecopymerge($dest, $src, 10, 9, 0, 0, 181, 180, 100); //have to play with these numbers for it to work for you, etc.
+//
+//        header('Content-Type: image/png');
+//        imagepng($dest);
+        imagepng($dest, "application/images/photos/file.png");
+//        debug($dest);
+//        imagedestroy($dest);
+//        imagedestroy($src);
     }
 
 

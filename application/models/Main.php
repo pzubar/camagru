811ec77645eac27fb77e1b6a7916825372804582
pzubar <pmzubar@gmail.com>
2019-02-13 22:38:49 +0200

    posts counter adjusted

--- application/models/Main.php
+++ application/models/Main.php
@@ -1,21 +1,29 @@
 <?php
-	
+
 	namespace application\models;
-	
+
 	use application\core\Model;
-	
-	class Main extends Model {
-	
+
+	class Main extends Model
+	{
+
 //		public function __construct()
 //		{
 //			parent::__construct();
 //			debug($this->db);
 //		}
-		
-		public function getPosts(string $page) {
+		public function getPostsNum()
+		{
+			$result = $this->db->column('SELECT COUNT(id)  FROM posts', []);
+			return $result;
+		}
+
+		public function getPosts(string $page)
+		{
 			$limit = 10;
 			$offset = (intval($page) - 1) * $limit;
-			$result = $this->db->row('SELECT posts.*, username FROM posts INNER JOIN users ON posts.author_id = users.id ORDER BY postdate DESC LIMIT ' . $limit . ' OFFSET '. $offset);
+			$result = $this->db->row('SELECT posts.*, username FROM posts INNER JOIN users ON posts.author_id = users.id ORDER BY postdate DESC LIMIT ' . $limit . ' OFFSET ' . $offset);
+
 			return $result;
 		}
 	}
--- application/views/main/index.php
+++ application/views/main/index.php
@@ -23,28 +23,9 @@
 
 </style>
 
-<?php
-	var_dump($val);
-	//	foreach ($posts as $val):?>
-<!--		<div class="container">-->
-<!--			<div class="row justify-content-md-center">-->
-<!--				<div class="card col-md-12 post-container">-->
-<!--					<div class="post-meta">-->
-<!--						<div class="post-author">-->
-<!--							--><?php //echo $val['username'] ?>
-<!--						</div>-->
-<!--						<span class="post-date">-->
-<!--							<small>-->
-<!--								--><?php //echo $val['postdate'] ?>
-<!--							</small>-->
-<!--						</span>-->
-<!--					</div>-->
-<!--					<img src='--><?php //echo $val['filename'] ?><!--'>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</div>-->
-<!--	--><?php //endforeach;
-?>
+<script>
+	window.postsNum = <?php    echo $postsNum    ?>;
+</script>
 <div id="wrapper" style="height: calc(100vh - 60px); overflow: auto;">
 	<div id="publications-container">
 	</div>

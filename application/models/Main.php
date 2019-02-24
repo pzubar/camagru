58b0266a9a3fa9172c16dbc4751085a5a43510a8
pmzubar <30j,tpmzy>
2019-02-25 00:48:51 +0200

    really huge query adjusted to take some info about posts

--- application/models/Main.php
+++ application/models/Main.php
@@ -14,20 +14,79 @@ class Main extends Model
 
     public function getPosts(string $page)
     {
-        $limit = 10;
-        $offset = (intval($page) - 1) * $limit;
-        $posts = $this->db->row('SELECT posts.*, username FROM posts INNER JOIN users ON posts.author_id = users.id ORDER BY postdate DESC LIMIT ' . $limit . ' OFFSET ' . $offset);
-        $comments = [];
-        foreach ($posts as $value) {
-            $comments[] = $this->db->row('SELECT * FROM comments WHERE post_id = :post_id', ['post_id' => $value['id']]);
-        }
+//        $limit = 10;
+//        $offset = (intval($page) - 1) * $limit;
+//        $posts = $this->db->row('SELECT posts.*, username FROM posts INNER JOIN users ON posts.author_id = users.id ORDER BY postdate DESC LIMIT ' . $limit . ' OFFSET ' . $offset);
+//        $comments = [];
+//        foreach ($posts as $value) {
+//            $comments[] = $this->db->row('SELECT * FROM comments WHERE post_id = :post_id', ['post_id' => $value['id']]);
+//        }
 
 //            $comments =  $this->db->row('SELECT *,  FROM comments INNER JOIN  ON comments.post_id = users.id ORDER BY postdate DESC LIMIT ' . $limit . ' OFFSET ' . $offset);
-        $result = [
-            'posts' => $posts,
-            'comments' => $comments
-        ];
+//        $result = [
+//            'posts' => $posts,
+//            'comments' => $comments
+//        ];
 //			return $result;
-        return $result;
+//        $result =
+//        return $result;
     }
 }
+
+/**
+ * SELECT
+POSTS_AND_COMMENTS.*,
+u.username AS cmt_author_name
+FROM (
+SELECT
+POSTS.*,
+c.author_id AS cmt_author_id,
+c.comment_text,
+c.postdate  AS cmt_postdate
+FROM (
+SELECT
+A.*,
+COUNT(l.id) AS 'count'
+FROM (
+SELECT
+p.*,
+username as author_name
+FROM posts p
+INNER JOIN users ON p.author_id = users.id
+ORDER BY postdate DESC
+LIMIT 10 OFFSET 0) A
+LEFT JOIN
+likes l ON A.id = l.post_id
+GROUP BY
+A.id) POSTS
+LEFT JOIN comments c ON POSTS.id = c.post_id
+) POSTS_AND_COMMENTS
+LEFT JOIN users u ON cmt_author_id = u.id
+
+ */
+
+/*
+ *
+ * SELECT
+  x.*,
+  u.username AS autor_name
+
+FROM (
+       SELECT
+         p.id,
+         p.filename,
+         p.postdate,
+         p.author_id      AS uid,
+         COUNT(c.post_id) AS comments_count
+
+       FROM
+         posts p
+         LEFT JOIN
+         comments c ON p.id = c.post_id
+       GROUP BY
+         p.id
+     ) x
+  LEFT JOIN
+  users u ON u.id = uid
+ORDER BY postdate DESC
+ */

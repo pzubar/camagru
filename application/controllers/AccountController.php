bf37356209e9036938392424ee5370d564164250
pmzubar <30j,tpmzy>
2019-02-24 12:54:20 +0200

    password reset adjusted

--- application/controllers/AccountController.php
+++ application/controllers/AccountController.php
@@ -1,83 +1,92 @@
 <?php
 
-	namespace application\controllers;
+namespace application\controllers;
 
-	use application\core\Controller;
-	use Exception;
+use application\core\Controller;
+use Exception;
 
-	class AccountController extends Controller
-	{
-		public function loginAction($message = null)
-		{
-			if (isset($_SESSION['logged_user']))
-				$this->view::redirect('/');
-			if (!empty($_POST) && !empty($_POST['login']) && !empty($_POST['password'])) {
-				try {
-					$this->model->loginUser($_POST['login'], $_POST['password']);
-				} catch (Exception $exception) {
-					$this->view->message('error', $exception->getMessage());
-				}
-				$this->view::redirect('/');
-			}
-			if ($message) {
-				if ($message === "check-email")
-					$message = "Check your email, we sent an account confirmation letter!";
-				else if ($message === "activation-success")
-					$message = "Your account was successfully activated";
-				else if ($message === "remind-success")
-					$message = "Check your email, we sent an instructions letter!";
-				else {
-					$this->view->errorCode(404);
-				}
-			}
-			$this->view->render('Sign in', ['message' => $message]);
-		}
+class AccountController extends Controller
+{
+    public function loginAction($message = null)
+    {
+        if (isset($_SESSION['logged_user']))
+            $this->view::redirect('/');
+        if (!empty($_POST) && !empty($_POST['login']) && !empty($_POST['password'])) {
+            try {
+                $this->model->loginUser($_POST['login'], $_POST['password']);
+            } catch (Exception $exception) {
+                $this->view->message('error', $exception->getMessage());
+            }
+            $this->view::redirect('/');
+        }
+        if ($message) {
+            if ($message === "check-email")
+                $message = "Check your email, we sent an account confirmation letter!";
+            else if ($message === "activation-success")
+                $message = "Your account was successfully activated";
+            else if ($message === "remind-success")
+                $message = "Check your email, we sent an instructions letter!";
+            else if ($message === "change-success")
+                $message = "Your email changed successfully";
+            else {
+                $this->view->errorCode(404);
+            }
+        }
+        $this->view->render('Sign in', ['message' => $message]);
+    }
 
-		public function registerAction()
-		{
-			if (isset($_SESSION['logged_user']))
-				$this->view::redirect('/');
-			if (!empty($_POST)) {
-				if (!empty($_POST['email']) && !empty($_POST['login'])) {
-					if ($this->model->checkRegisterData($_POST['email'], $_POST['login'])) {
-						$this->view->message('fail', 'User with such a login or email already exists');
-					} else {
-						$this->model->createUser($_POST['email'], $_POST['login'], $_POST['password']);
-						$this->view->redirect('/account/login/check-email');
-					}
-				}
-			}
-			$this->view->render('Sign up');
-		}
+    public function registerAction()
+    {
+        if (isset($_SESSION['logged_user']))
+            $this->view::redirect('/');
+        if (!empty($_POST)) {
+            if (!empty($_POST['email']) && !empty($_POST['login'])) {
+                if ($this->model->checkRegisterData($_POST['email'], $_POST['login'])) {
+                    $this->view->message('fail', 'User with such a login or email already exists');
+                } else {
+                    $this->model->createUser($_POST['email'], $_POST['login'], $_POST['password']);
+                    $this->view->redirect('/account/login/check-email');
+                }
+            }
+        }
+        $this->view->render('Sign up');
+    }
 
-		public function authAction($hash = null)
-		{
-			if ($hash) {
-				$this->model->activateUser($hash);
-				$this->view->redirect('/account/login/activation-success');
-			} else
-				$this->view::redirect('/');
-		}
+    public function authAction($hash = null)
+    {
+        if ($hash) {
+            $this->model->activateUser($hash);
+            $this->view->redirect('/account/login/activation-success');
+        } else
+            $this->view::redirect('/');
+    }
 
-		public function remindPasswordAction($hash = null)
-		{
-			if (isset($_SESSION['logged_user']))
-				$this->view::redirect('/');
-			if ($hash) {
-				$this->view->render('Reset password', ['isset_hash' => true]);
-			}
+    public function remindPasswordAction($hash = null)
+    {
+        if (isset($_SESSION['logged_user']))
+            $this->view::redirect('/');
+        if (isset($_POST['password']) && isset($_POST['hash'])) {
+            try {
+                $this->model->changePassword($_POST['hash'], $_POST['password']);
+                $this->view::redirect('/account/login/change-success');
+//                $this->view->message('redirect', 'success');
+            } catch (Exception $exception) {
+                $this->view->message('error', $exception->getMessage());
+            }
+        }
+        if ($hash) {
+            $this->view->render('Reset password', ['isset_hash' => true, 'hash' => $hash]);
+        }
+        if (!empty($_POST['email'])) {
+            try {
+                $this->model->remindPassword($_POST['email']);
+            } catch (Exception $exception) {
+                $this->view->message('error', $exception->getMessage());
+            }
+            $this->view::redirect('/account/login/remind-success');
+        }
+        $this->view->render('Reset password');
+    }
 
-			if (!empty($_POST['email'])) {
-				try {
-					$this->model->remindPassword($_POST['email']);
-				}
-				catch (Exception $exception) {
-					debug($exception);
-				}
-				$this->view::redirect('/account/login/remind-success');
-			}
-			$this->view->render('Reset password');
-		}
 
-
-	}
+}
--- application/views/account/remindPassword.php
+++ application/views/account/remindPassword.php
@@ -1,24 +1,77 @@
 <style>
-	.remind-form {
-		height: 100vh;
-		display: flex;
-		align-items: center;
-		margin-top: -55px;
-	}
+    .remind-form {
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        margin-top: -55px;
+    }
 </style>
+<? if (isset($vars['isset_hash'])): ?>
+    <div class="container">
+        <div class="row justify-content-md-center remind-form">
+            <div class="col-md-auto">
+                <h3>Remind password</h3>
+                <p style="margin-bottom: 0">Enter new password you would like to use</p>
+                <form action="/account/remind-password" method="post" id="change-password-form">
+                    <div class="form-group">
+                        <input class="form-control" id="password" name="password" placeholder="Password" type="password"
+                               required>
+                    </div>
+                    <input id="hash" name="hash" type="hidden" value="<? if (isset($vars)) {
+                        echo $vars['hash'];
+                    } ?>">
+                    <button class="btn btn-info" type="submit">Change password</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script>
+        const formChangePass = document.querySelector('#change-password-form');
+        if (formChangePass)
+            formChangePass.addEventListener("submit", function (e) {
+                e.preventDefault();
 
-<div class="container">
-	<div class="row justify-content-md-center remind-form">
-		<div class="col-md-auto">
-			<h3>Remind password</h3>
-			<p style="margin-bottom: 0">Enter e-mail and we'll send you a letter with instructions</p>
-			<form action="/account/remind-password" method="post" id="register-form">
-				<div class="form-group">
-					<label for="email"></label>
-					<input class="form-control" id="email" type="text" name="email" placeholder="E-mail" required>
-				</div>
-				<button class="btn btn-info" type="submit">Reset password</button>
-			</form>
-		</div>
-	</div>
-</div>
+                fetch(this.action, {
+                    method: this.method,
+                    body: new FormData(this),
+                    redirect: "follow"
+                })
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw "Response status was not ok: " + response.status;
+                        }
+                        if (response.redirected) {
+                            const url = response.url;
+
+                            window.location.replace(url);
+                        }
+                        else
+                            return response.json();
+                    })
+                    .then(function (response) {
+                        if (response['status'] && response['status'] !== "success")
+                            alert(response.message)
+                    })
+                    .catch(function (err) {
+                        console.log('Fetch Error. ', err);
+                    });
+            }
+    </script>
+
+<? else: ?>
+    <div class="container">
+        <div class="row justify-content-md-center remind-form">
+            <div class="col-md-auto">
+                <h3>Remind password</h3>
+                <p style="margin-bottom: 0">Enter e-mail and we'll send you a letter with instructions</p>
+                <form action="/account/remind-password" method="post" id="register-form">
+                    <div class="form-group">
+                        <label for="email"></label>
+                        <input class="form-control" id="email" type="text" name="email" placeholder="E-mail" required>
+                    </div>
+                    <button class="btn btn-info" type="submit">Reset password</button>
+                </form>
+            </div>
+        </div>
+    </div>
+<?php endif ?>
